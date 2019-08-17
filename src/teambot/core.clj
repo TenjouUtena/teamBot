d9c1c47e50776ee03d276047d8e3e8efe3f397fa
Brendan Speer <brendan.speer@gmail.com>
2019-08-17 18:30:56 -0500

    * Setup

--- src/teambot/core.clj
+++ src/teambot/core.clj
@@ -1,6 +1,15 @@
-(ns teambot.core)
+(ns teambot.core
+
+  (:require
+    [discord.bot
+     :as
+     bot]))
 
 (defn foo
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
+
+
+(defn startbot []
+  (bot/start))
